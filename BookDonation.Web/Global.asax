b4985300974cbf0363c7dd4bb17b42d8db78e3e7
johnguggy <42472160+johnguggy@users.noreply.github.com>
2018-11-08 13:45:30 -0500

    Initial Skeleton

---
+++ BookDonation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookDonation.Web.MvcApplication" Language="C#" %>
---
+++ BookDonation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookDonation.Web.Startup))]
+namespace BookDonation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
